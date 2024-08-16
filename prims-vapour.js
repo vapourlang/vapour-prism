533875a4640ee9146f29acc233c1456cf3c59660
John Coene <jcoenep@gmail.com>
2024-08-16 20:29:08 +0200

    add let and const

--- prims-vapour.js
+++ prims-vapour.js
@@ -16,7 +16,7 @@ Prism.languages.vapour = {
 		/\b(?:Inf|NaN)\b/,
 		/(?:\b0x[\dA-Fa-f]+(?:\.\d*)?|\b\d+(?:\.\d*)?|\B\.\d+)(?:[EePp][+-]?\d+)?[iL]?/
 	],
-	'keyword': /\b(?:NA|type|func|int|char|num|struct|dataframe|object|any|na|null|matrix|list|return|NA_character_|NA_complex_|NA_integer_|NA_real_|NULL|break|else|for|function|if|in|next|repeat|while)\b/,
+	'keyword': /\b(?:NA|let|const|type|func|int|char|num|struct|dataframe|object|any|na|null|matrix|list|return|NA_character_|NA_complex_|NA_integer_|NA_real_|NULL|break|else|for|function|if|in|next|repeat|while)\b/,
 	'operator': /->?>?|<(?:=|<?-)?|[>=!]=?|::?|&&?|\|\|?|[+*\/^$@~]/,
 	'punctuation': /[(){}\[\],;]/
 };
